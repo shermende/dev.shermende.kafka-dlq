eb65c1ddabf08e0f7461f1073a176def7e11553b
abdys <n.u.abdysamat@gmail.com>
2020-11-02 00:48:53 +0600

    prepare project

--- src/main/java/dev/shermende/kafkaretry/KafkaRetryApplication.java
+++ src/main/java/dev/shermende/kafkaretry/KafkaRetryApplication.java
@@ -2,11 +2,8 @@ package dev.shermende.kafkaretry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Processor;
 
 @SpringBootApplication
-@EnableBinding(Processor.class)
 public class KafkaRetryApplication {
 
     public static void main(String[] args) {
